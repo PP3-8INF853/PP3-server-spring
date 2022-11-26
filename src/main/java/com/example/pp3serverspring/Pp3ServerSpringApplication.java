99d335d014fc899e15ba3a6a332508f5635b84f1
Asa <asareel56@gmail.com>
2022-11-26 16:41:12 -0500

    init repo

---
+++ src/main/java/com/example/pp3serverspring/Pp3ServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp3serverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp3ServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pp3ServerSpringApplication.class, args);
+    }
+
+}
